b58ab3e11263cdb042ed7edbd5ca0af17a046af9
LKrAxViCt0R <108606699+LKrAxViCt0R@users.noreply.github.com>
2023-03-10 03:55:33 +0530

    Update LessonTwoChallenge.sol

--- LessonTwoChallenge.sol
+++ LessonTwoChallenge.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 contract LessonTwoChallenge{
-    function returnValue() public payable returns(uint256 valueEther,uint256 valueWei,uint256 valueGwei){
+    function returnValue() public payable returns(uint valueEther,uint valueWei,uint valueGwei){
         valueWei = msg.value;
         valueEther = valueWei / 1 ether;
         valueGwei = valueWei / 1 gwei;
